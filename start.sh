7c1952b0526619e46f9f00ab9c507a1dd3f86522
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-11-04 17:31:03 +0300

    Make few changes.

--- start.sh
+++
@@ -1,6 +0,0 @@
-export MOVIE_API_KEY='b395a1f93e3494e9c9ee4328204b609c'
-export SECRET_KEY='FlaskWTFSecretKey'
-export MAIL_USERNAME='gidaliosventures@gmail.com'
-export MAIL_PASSWORD='gideon1284'
-
-python3.9 manage.py server
