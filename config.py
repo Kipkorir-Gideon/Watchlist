357ea6f6953180a19d694f075c32c7c6ff0c144a
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-11-04 17:25:35 +0300

    Add markdown.

--- config.py
+++ config.py
@@ -18,14 +18,24 @@ class Config:
     SENDER_EMAIL = 'gidaliosventures@gmail.com'
 
 
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+
 class ProdConfig(Config):
     pass
 
 
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://access:gideon.@localhost/watchlist_test'
+
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://access:gideon.@localhost/watchlist'
     DEBUG = True
 
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test':TestConfig
 }
